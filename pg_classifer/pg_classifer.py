2505206247ea58d497659e1a804443b1ea88cee5
chunyuan.wu <chunyuan.wu@iherb.com>
2022-02-16 16:26:47 +0800

    using tensorflow tokenizer and padding

--- pg_classifer/pg_classifer.py
+++ pg_classifer/pg_classifer.py
@@ -1,9 +1,27 @@
 import numpy as np
 import pandas as pd
+import re
+import tensorflow as tf
+from keras import layers
 
-def load_raw_data():
-    #return np.fromregex('./data/raw.csv', r'"(\\w+)","?(\\w+)"?', np.object)
-    return pd.read_csv('./data/raw.csv', quotechar='"', header=None).to_numpy()
+def standardize(val):
+    return re.sub(r',|\+|\(|\)|\d|\.|/|-', ' ', val).lower().split()
+
+def standardize_y(rawData):
+    d = {}
+    idx = 0
+    for i in range(len(rawData)):
+        r = rawData[i]
+        y = rawData[i][1]       
+        if y not in d:
+            d[y] = idx
+            
+        rawData[i][1] = d[y]
+        idx += 1
+
+    return {v: k for k, v in d.items()}
+
+
 
 def split_data(rawData):
     num = rawData.shape[0]
@@ -14,3 +32,13 @@ def split_data(rawData):
         (rawData[train_num:train_num + test_num, 0], rawData[train_num:train_num + test_num, 1]),
         (rawData[train_num + test_num:, 0], rawData[train_num + test_num:, 1]),
     ]
+
+def create_vocabulary(rawData):
+    voc = set()
+
+    for s in rawData[:, 0]:
+        for w in standardize(s):
+            if w != '':
+                voc.add(w)
+    
+    return list(voc)
